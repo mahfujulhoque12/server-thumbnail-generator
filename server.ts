7c4949c8820ddafe701f910cf30f74d078c98cc1
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2026-01-17 00:45:32 +0600

    add

--- server.ts
+++ server.ts
@@ -41,3 +41,5 @@ const port = process.env.PORT || 7000; // fallback if .env fails
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+export default app;
